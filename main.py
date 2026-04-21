ce1b1463c818eb7a21af5b44c73f501b351270f0
Ataxanov <asherzod2197@gmail.com>
2026-04-21 20:11:35 +0500

    Add PasswordManager class with strength check

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+class PasswordManager:
+    def __init__(self, password):
+        self.password = password
 
+    def check_strength(self):
+        if len(self.password) >= 8:
+            print("Kuchli parol")
+        else:
+            print("Zaif parol")
+
+    def change(self, new_password):
+        self.password = new_password
+        print("Parol o‘zgartirildi")
+
+
+# Obyekt
+pm = PasswordManager("12345")
+pm.check_strength()
+pm.change("strong123")
+pm.check_strength()
